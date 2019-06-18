ec783b66b5651d103abc30ea3ba7f906d590e26c
argostellar <arskud@gmail.com>
2019-06-18 22:05:27 +0300

    Внёс исправления ошибок линтера

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,18 @@
-var renderStatistics = function(ctx, names, times) {
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
   // Вывожу тень облака
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'
-  ctx.fillRect(110,20,530,290);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 530, 290);
   // Вывожу само облако
   ctx.fillStyle = 'white';
-  ctx.fillRect(100,10,520,280);
+  ctx.fillRect(100, 10, 520, 280);
   // Пишу основную надпись
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!\n', 120, 30);
-  ctx.fillText('Список результатов:', 120, 50)
+  ctx.fillText('Список результатов:', 120, 50);
   // Создаю цикл для отрисовки гистограммы
   for (var k = 0; k < times.length; k++) {
     // Завожу значение для хранения максимального значения
@@ -27,18 +29,15 @@ var renderStatistics = function(ctx, names, times) {
     }
     // Завожу переменные для процента высоты от максимального значения, так как
     // значение высоты гистограмы равно 150 * процент высоты от максимального значения
-    console.log('Это максимальное значение: ' + maxResult);
-    console.log('Текущее значение итерации: ' + times[k]);
+
+
     var percentOfHeight = (100 * times[k]) / maxResult;
-    console.log('Процент от максимальной высоты: ' + percentOfHeight);
+
     var histogramHeight = Math.floor(150 * (percentOfHeight / 100));
     if (histogramHeight > 150) {
       histogramHeight = 150;
-      console.log('Произошла ошибка рассчётов высоты');
+
     }
-
-    console.log('Высота гистограммы: ' + histogramHeight);
-
     // Завожу переменную для координаты Х, которая будет меняться с каждой итерацией цикла
     // 150 - базовая координата, 90 - сумма ширины гистограммы (40) и пробела между ними (50)
     var coordinateX = 150 + 90 * k;
@@ -52,9 +51,6 @@ var renderStatistics = function(ctx, names, times) {
     ctx.lineTo(coordinateX, coordinateY);
     ctx.closePath();
     ctx.stroke();
-
-    var randomSaturation = Math.random();
-    console.log('Текущее значение насыщенности: ' + randomSaturation);
     // Завожу проверку, если значение k-элемента равно 'Вы', меняю стиль заливки на красный
     if (names[k] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -72,4 +68,4 @@ var renderStatistics = function(ctx, names, times) {
     ctx.fillText(names[k], coordinateX, coordinateY + 10);
   }
 
-}
+};
