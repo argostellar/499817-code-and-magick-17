191d9a4cc1979468cfd88f599caf7eafdff327bc
argostellar <arskud@gmail.com>
2019-07-16 16:27:36 +0300

    Выполнение задания 6

---
+++ js/backend.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// backend.js - модуль отвечает за загрузку/отправку данных с/на сервера
+(function () {
+  var GET_URL = 'https://js.dump.academy/code-and-magick/data';
+  var POST_URL = 'https://js.dump.academy/code-and-magick';
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.open('POST', POST_URL);
+    xhr.send(data);
+  };
+
+  var load = function (onLoad, onError) {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', GET_URL);
+    xhr.send();
+  };
+
+  window.frontend = {
+    dzero: 'dzero'
+  };
+
+  window.backend = (function () {
+    return {
+      save: save,
+      load: load
+    };
+  })();
+})();
